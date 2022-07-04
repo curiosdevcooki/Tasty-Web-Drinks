1844649bcb3a9e034d5d69b019c5db555ce38a83
Ariadne Engelbrecht <ariadne.engelbrecht@thenativeweb.io>
2022-07-04 20:14:32 +0200

    Add ingredients of Gin Tonic Web Component

--- gin tonic/gin-tonic.js
+++ gin tonic/gin-tonic.js
@@ -1,14 +1,33 @@
 //Create tasty, stand-alone Web Component of your drink:
 
 class GinTonic extends HTMLElement {
-    constructor() {
-        super();
-        const html = element.getInnerHTML({
-            includeShadowRoots: true
-        });
-        html.appendChild(element.content.cloneNode(true))   ;
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: 'open'
+    });
+    this.shadowRoot.innerHTML = `
+      <style>
+        ul {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }s
+      </style>
+      <article>
+        <h1>Gin Tonic</h1>
+        <ul>
 
-    }
+        <li>Cubes of ice: 3</li>
+        <li>Lime sclices: 3</li>
+        <li>Gin: 2 parts</li>
+        <li>Tonic: 5 parts</li>
+  
+        </ul>  
+        </description>
+      </article>
+      `;
+  }
 }
 
 customElements.define('gin-tonic', GinTonic);
